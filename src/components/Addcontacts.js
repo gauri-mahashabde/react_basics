61b143c8a6ccc62f7fcb0ebb77c4a0e6c569fae1
Gauri Mahashabde <gauri2interact@gmail.com>
2022-11-19 22:39:57 +0530

    components added

---
+++ src/components/Addcontacts.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Addcontacts = () => {
+    return(
+        <div className="ui main">
+            <h2>Add Contacts</h2>
+                <form className="ui form">
+                    <div className="field">
+                        <label>Name</label> 
+                        <input type="text" name="name" placeholder="Name"/>
+                    </div>
+                    <div className="field">
+                        <label>Email</label>
+                        <input type="text" name="email" placeholder="email"/>
+                    </div>
+                    <button className="ui button blue">Add</button>
+                </form>
+        </div>
+    );
+}
+
+
+export default Addcontacts;
---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+import Addcontacts from './Addcontacts';
+
+function App() {
+  return (
+    
+    <div className="ui conatiner">
+    <Header/>
+    <Addcontacts/>
+    </div>
+
+);
+    }
+
+export default App;
+
+    
+    
+ 
